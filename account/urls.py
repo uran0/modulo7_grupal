1d2ae2c4fa11e352aaa523423dcf2faea5c9d115
Johanna Cid <cid.joa@gmail.com>
2024-06-30 17:24:51 -0400

    ejercicio 4

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomLoginView, welcome
+from .views import CustomLoginView, welcome, register
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('welcome/', welcome, name='welcome'),
+    path('register/', register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
 class CustomLoginView(LoginView):
     template_name = 'account/login.html'
+    success_url = reverse_lazy('welcome')
 
 @login_required
 def welcome(request):
@@ -16,3 +20,14 @@ def welcome(request):
         return render(request, 'staff_welcome.html', {'user': user})
     else:
         return render(request, 'user_welcome.html', {'user': user})
+    
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'account/register.html', {'form': form})
